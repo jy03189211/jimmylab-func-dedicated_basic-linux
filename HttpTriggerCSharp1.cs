a1dee6ef3f44e2f4255ae5ca53f6c9e3193d9112
timberjj <5427963+timberjj@user.noreply.gitee.com>
2021-05-06 23:30:32 +0800

    scale down to basic

--- HttpTriggerCSharp1.cs
+++ HttpTriggerCSharp1.cs
@@ -17,7 +17,7 @@ namespace Company.Function
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("this is a basic plan");
 
             string name = req.Query["name"];
 
